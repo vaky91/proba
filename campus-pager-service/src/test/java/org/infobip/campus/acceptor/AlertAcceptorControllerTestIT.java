4ddb815a0cac12c2ecb652e481c177afa81937f1
Ivica Obestar <ivica.obestar@infobip.com>
2016-08-11 12:24:00 +0200

    cleanup

---
+++ campus-pager-service/src/test/java/org/infobip/campus/acceptor/AlertAcceptorControllerTestIT.java
@@ -0,0 +1,48 @@
+package org.infobip.campus.acceptor;
+
+import org.infobip.campus.Application;
+import org.infobip.campus.notifier.Gateway;
+import org.infobip.campus.notifier.NotificationSender;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
+
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * @author iobestar
+ * @since 11.08.2016. 12:08
+ */
+@WebIntegrationTest("spring.profiles.active=test")
+@SpringApplicationConfiguration(value = {Application.class, AlertAcceptorControllerTestIT.TestConfiguration.class})
+public class AlertAcceptorControllerTestIT {
+
+    @ClassRule
+    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+
+    @Rule
+    public final SpringMethodRule springMethodRule = new SpringMethodRule();
+
+    // REMOVE THIS - SHOULD BE RESOLVED WITH WIREMOCK!!
+    public static class TestConfiguration {
+
+        @Bean
+        public Gateway gateway() {
+            return mock(Gateway.class);
+        }
+
+    }
+
+    @Test
+    public void acceptAlert() throws Exception {
+        // WRITE INTEGRATION TEST HERE
+    }
+
+}
