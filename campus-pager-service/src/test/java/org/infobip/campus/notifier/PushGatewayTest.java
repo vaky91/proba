149c90b2a6e36181ff17c3bf27445a93918ba85d
vaky91 <bilicvalter@gmail.com>
2016-08-12 22:17:54 +0200

    Update PushGatewayTest.java
    Rešeno bre

--- campus-pager-service/src/test/java/org/infobip/campus/notifier/PushGatewayTest.java
+++ campus-pager-service/src/test/java/org/infobip/campus/notifier/PushGatewayTest.java
@@ -24,16 +24,16 @@ public class PushGatewayTest {
                         .withBody("{'response':'OK'}")));
 
         Gateway gateway = new PushGateway("http://localhost:8080/push",
-                "6dad0991ff524d69f84cd1ae07683895-cf5fc25c-9334-4e9a-bcf6-df8301bb1b6d",
-                "b7b1fc06983b3dff0aabece3973951f3-23e80b14-f291-4d6d-8af7-5146850d7602");
-        gateway.push(Collections.singletonList(new GatewayRequest(new Member("13373420"), "Probni push")));
+        <"api-key">,
+                <"from some user">);
+        gateway.push(Collections.singletonList(new GatewayRequest(new Member(<number>), "Probni push")));
 
         verify(postRequestedFor(urlMatching("/push"))
-                .withHeader("Authorization", equalTo("App b7b1fc06983b3dff0aabece3973951f3-23e80b14-f291-4d6d-8af7-5146850d7602"))
+                .withHeader("Authorization", equalTo(<"authorization">))
                 .withHeader("Content-Type", equalTo("application/json"))
                 .withHeader("Accept", equalTo("application/json"))
-                .withRequestBody(equalTo("{\"from\":\"6dad0991ff524d69f84cd1ae07683895-cf5fc25c-9334-4e9a-bcf6-df8301bb1b6d\"," +
-                        "\"to\":\"13373420\"," +
+                .withRequestBody(equalTo("{\"from\":<\"from some user\">," +
+                        "\"to\":<\"some number\">," +
                         "\"text\":\"Probni push\"}")));
     }
 }
